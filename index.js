c78533d941cc3b6156fdf15575c5ea40b61a13c1
Monica Mateiu <monica.mateiu@gmail.com>
2019-04-07 13:36:10 +0100

    setup index.js and Header component

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a Component
+const App = () => (
+  <Text>Some text</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
