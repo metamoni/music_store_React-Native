b8ad22d3ba2332640a54b92612a1318628362baf
Monica Mateiu <monica.mateiu@gmail.com>
2019-04-09 16:59:32 +0100

    create Button component and add to AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { thumbnail_image, title, artist, image } = album;
@@ -27,11 +28,16 @@ const AlbumDetail = ({ album }) => {
         <Text>{artist}</Text>
       </View>
       </CardSection>
+
       <CardSection>
         <Image
          source={{ uri: image }}
          style={imageStyle}
         />
+      </CardSection>
+      
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Button = () => (
+  <Text>Button</Text>
+)
+
+export default Button;
